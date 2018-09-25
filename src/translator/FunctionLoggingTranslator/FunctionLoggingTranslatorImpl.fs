386d3801c03b6de683035bd941a09e3d0e885d5f
Kouji Matsui <k@kekyo.net>
2018-09-25 20:51:20 +0900

    Write test code.

---
+++ src/translator/FunctionLoggingTranslator/FunctionLoggingTranslatorImpl.fs
@@ -0,0 +1,16 @@
+﻿namespace FunctionLoggingTranslator
+
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Compiler.Configurations
+open Microsoft.FSharp.Compiler.Ast
+
+type FunctionLoggingTranslatorImpl() = 
+    interface ITranslator<ICompilerConfig, ParsedInput> with
+        member __.Name = "FunctionLoggingTranslator"
+        member __.Translate config input =
+            printfn "FunctionLoggingTranslator: HelloTranslator."
+            printfn "%A\n" input
+            input
+
+[<assembly: Translator(typeof<FunctionLoggingTranslatorImpl>)>]
+do ()
